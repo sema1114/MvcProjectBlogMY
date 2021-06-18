f0bdcdc6e3cbbe4954feca62a3fa626ed88c29c0
Sema Kaya <semabayri00@gmail.com>
2021-06-18 20:39:27 +0200

    Default Controller part 1

---
+++ MvcProjeKampi/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using BusinessLayer.Concrate;
+using DataAccessLayer.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class DefaultController : Controller
+    {
+        HeadingManager hm = new HeadingManager(new EfHeadingDal());
+        public ActionResult Headings()
+        {
+            var headingList = hm.List();
+            return View(headingList);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
