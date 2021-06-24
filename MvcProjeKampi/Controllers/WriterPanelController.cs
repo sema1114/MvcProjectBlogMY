c5be76b5d6a7bc69550a9206ea3c46269eafa6b3
Sema Kaya <semabayri00@gmail.com>
2021-06-24 12:03:49 +0200

    Writer Panel,_WriterLayout Edited

--- MvcProjeKampi/Controllers/WriterPanelController.cs
+++ MvcProjeKampi/Controllers/WriterPanelController.cs
@@ -9,6 +9,8 @@ using System.Web;
 using System.Web.Mvc;
 using PagedList;
 using PagedList.Mvc;
+using FluentValidation.Results;
+using BusinessLayer.ValidationRules;
 
 namespace MvcProjeKampi.Controllers
 {
@@ -18,13 +20,47 @@ namespace MvcProjeKampi.Controllers
     {
         HeadingManager hm = new HeadingManager(new EfHeadingDal());
         CategoryManager cm = new CategoryManager(new EfCategoryDal());
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        WriterValidator writerValidator = new WriterValidator();
         Context c = new Context();
        
         // GET: WriterPanel
-        public ActionResult WriterProfile()
+
+        [HttpGet]
+        public ActionResult WriterProfile(int id=0)
         {
-            return View();
+            string p = (string)Session["WriterMail"];
+            id = c.Writers.Where(x => x.WriterMail == p).Select(y => y.WriterID).FirstOrDefault();
+            var writervalue = wm.GetByID(id);
+
+            return View(writervalue);
         }
+
+        [HttpPost]
+        public ActionResult WriterProfile(Writer writer)
+        {
+            ValidationResult results = writerValidator.Validate(writer);
+
+            if (results.IsValid)
+            {
+                wm.Update(writer);
+                return RedirectToAction("AllHeading");
+
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+
+            return View();
+
+        }
+
+
 
         public ActionResult MyHeading(string p)
         {
